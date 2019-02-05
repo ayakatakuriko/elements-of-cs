25962960def01baeefa28235b0d16438b1710e45
ayakatakuriko <39085632+ayakatakuriko@users.noreply.github.com>
2019-02-05 15:37:28 +0900

    CPU Finished!!

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -6,9 +6,44 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+  @8192
+  D=A
+  @count
+  M=D
+(LOOP)
+  @index
+  M=0
+(INNER)
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ //M[24576]=0なら画面を白くする
+(BLACK)
+  @index
+  D=M
+  @SCREEN
+  A=A+D
+  M=-1
+  @END
+  0;JMP
+(WHITE)
+  @index
+  D=M
+  @SCREEN
+  A=A+D
+  M=0
+(END)
+  @index
+  MD=M+1
+  @count
+  D=D-M
+  @LOOP
+  D;JEQ
+  @INNER
+  0;JMP
