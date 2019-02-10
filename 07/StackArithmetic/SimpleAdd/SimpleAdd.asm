62e990ee38e4e8e892e9e6dfd5a4bc7e3b789bd1
ayakatakuriko <39085632+ayakatakuriko@users.noreply.github.com>
2019-02-10 15:30:40 +0900

    add Memory Game

---
+++ 07/StackArithmetic/SimpleAdd/SimpleAdd.asm
@@ -0,0 +1,27 @@
+	@7
+	D=A
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+	@8
+	D=A
+	@SP
+	A=M
+	M=D
+	@SP
+	M=M+1
+	@SP
+	M=M-1
+	A=M
+	D=M
+	@SP
+	M=M-1
+	A=M
+	M=M+D
+	@SP
+	M=M+1
+(END_INFINIT_LOOP)
+	@END_INFINIT_LOOP
+	0;JMP
